f226b0fed37e2fefe6fe8750def2f83d25a613a5
Enrique Gallardo <62443583+enriquegallardov@users.noreply.github.com>
2021-04-04 19:18:44 -0500

    added sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `Candy Sweet`,
     description: `Candy Sweet esta dedicado a proveer los mejores productos azucarados`,
     author: `Enrique Gallardo`,
+    siteUrl: `https://candysweet.gatsbyjs.io`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -37,6 +38,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
